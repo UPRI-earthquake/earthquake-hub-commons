e88f78abb5cde76b7901fdbbf45e0d7bf3a86804
Jeff Sanchez (sysop@hp) <sanchez.christopherjeff@gmail.com>
2023-04-25 22:02:33 +0800

    chore: remove auto-run scripts in favor of manual script

--- deploymentTesting/mongodb/create-db.js
+++
@@ -1,5 +0,0 @@
-// Create DB and collections
-db = new Mongo().getDB("latestEQs");
-db.createCollection("accounts", { capped: false });
-db.createCollection("accountdetails", { capped: false });
-db.createCollection("devices", { capped: false });
