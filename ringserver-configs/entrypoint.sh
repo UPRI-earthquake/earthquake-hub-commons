d2e3d20d35c8ecfd29310f4b959e5821483b57ad
prokorpio@hp <sanchez.christopherjeff@gmail.com>
2023-07-12 23:20:46 +0800

    refactor: separate checks for each config file or dir

--- ringserver-configs/entrypoint.sh
+++ ringserver-configs/entrypoint.sh
@@ -6,17 +6,27 @@ ring_dir="${config_dir}/ring"
 auth_dir="${config_dir}/auth"
 
 # Check if the necessary files and directories exist
-if [[ ! -f "${ring_conf_file}" && ! -d "${ring_dir}" && ! -d "${auth_dir}" ]]; then
-  echo "Initializing configuration files and directories..."
-  mkdir -p "${config_dir}"
-  mv /app/ring.conf "${ring_conf_file}"
-  mv /app/ring "${ring_dir}"
-  mv /app/auth "${auth_dir}"
+if [[ ! -f "${ring_conf_file}" ]]; then
+  echo "Initializing ring.conf file..."
+  cp /app/ring.conf "${ring_conf_file}"
 
-  # Modify ring.conf file
+  # Modify ring.conf file (since dirs are moved)
   sed -i 's#RingDirectory ring#RingDirectory host-configs/ring#' "${ring_conf_file}"
   sed -i 's#AuthDir auth#AuthDir host-configs/auth#' "${ring_conf_file}"
+
 fi
+
+if [[ ! -d "${ring_dir}" ]]; then
+  echo "Initializing ring directory..."
+  cp /app/ring "${ring_dir}"
+fi
+
+if [[ ! -d "${auth_dir}" ]]; then
+  echo "Initializing auth directory..."
+  cp /app/auth "${auth_dir}"
+fi
+
+echo "Configuration files and directories initialized."
 
 # Run ringserver
 /app/ringserver -vv "${ring_conf_file}"
